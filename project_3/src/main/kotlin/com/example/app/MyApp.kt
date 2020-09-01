d08ebdc556f7456d287361a936e7207bb08111a3
Otto <ottofernandes2012@gmail.com>
2020-09-01 11:22:32 -0300

    New project added

---
+++ project_3/src/main/kotlin/com/example/app/MyApp.kt
@@ -0,0 +1,17 @@
+package com.example.app
+
+import com.example.view.MainView
+import javafx.stage.Stage
+import tornadofx.App
+
+class MyApp: App(MainView::class, Styles::class) {
+    override fun start(stage: Stage) {
+
+        with(stage) {
+            width = 500.0
+            height = 500.0
+        }
+
+        super.start(stage)
+    }
+}
